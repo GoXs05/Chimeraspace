14462cea323e0015d2bb5d2a337fe24e1774ed13
GoXs05 <gokulsriram992005@gmail.com>
2023-09-12 12:57:28 -0500

    Player Movement & Guns - Bug Fixes

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -6,13 +6,17 @@ public class PlatformMovement : MonoBehaviour
 {
     private Vector3 startPos;
 
+    public float freqFactor;
+    public float amplitude;
+
     void Start()
     {
         startPos = transform.position;
+        freqFactor = Random.Range(freqFactor - 0.1f, freqFactor + 0.1f);
     }
 
     void Update()
     {
-        transform.position = startPos + new Vector3(0f, Mathf.Sin(Time.time / 1.5f), 0f);
+        transform.position = startPos + new Vector3(0f, amplitude * Mathf.Sin(Time.time * freqFactor), 0f);
     }
 }
